4d2323f3da36c24fb8073760baa2d9a3c4ec7649
メガンテ <67883233+mgntpk@users.noreply.github.com>
2020-11-13 15:15:37 +0900

    Update onclick_p.js

--- js/onclick_p.js
+++ js/onclick_p.js
@@ -1,6 +1,6 @@
 function onclick_p_word_list(num1,num2){
-    used.value = word_list[num1][num2][0];
+    document.getElementById("used_word").value = word_list[num1][num2][0];
 }
 function onclick_p_used_list(num1,num2){
-    returner.value = word_list[num1][num2][0];
+    document.getElementById("return_word").value = word_list[num1][num2][0];
 }
